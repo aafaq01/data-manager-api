0f7b737270718de6ad3f4dda4001f66d1f470320
Aafaq Rana <m.aafaq.rana@gmail.com>
2024-06-09 15:56:37 +0500

    Update Profile get endpoint and added

--- src/routers/profile.py
+++ src/routers/profile.py
@@ -1,30 +1,52 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy.orm import Session
-from sqlalchemy.future import select
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session, selectinload
 from models.profile import Profile as ProfileModel
 from ..database import get_db
-from ..schemas.profile import Profile as ProfileSchema
+from ..schemas.profile import Profile, ProfileCreate, ProfileDetail
 
 router = APIRouter()
 
-@router.get("/profiles", response_model=list[ProfileSchema])
-def read_profiles(
-    skip: int = 0,
-    limit: int = 10,
-    name: str = Query(None),
-    db: Session = Depends(get_db)
-):
-    query = db.query(ProfileModel)
-    
-    if name:
-        query = query.filter(ProfileModel.name.ilike(f"%{name}%"))
-
-    profiles = query.offset(skip).limit(limit).all()
+@router.get("/profiles", response_model=list[Profile])
+def read_profiles(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    profiles = db.query(ProfileModel).offset(skip).limit(limit).all()
     return profiles
 
-@router.get("/profiles/{profile_id}", response_model=ProfileSchema)
+@router.get("/profiles/{profile_id}", response_model=Profile)
 def read_profile(profile_id: int, db: Session = Depends(get_db)):
     profile = db.query(ProfileModel).filter(ProfileModel.id == profile_id).first()
+    
     if not profile:
         raise HTTPException(status_code=404, detail="Profile not found")
+    
     return profile
+
+@router.get("/profiles/{profile_id}/details", response_model=ProfileDetail)
+def read_profile_details(profile_id: int, db: Session = Depends(get_db)):
+    profile = db.query(ProfileModel).options(
+        selectinload(ProfileModel.certifications),
+        selectinload(ProfileModel.contacts),
+        selectinload(ProfileModel.past_experiences),
+        selectinload(ProfileModel.programming_skills),
+        selectinload(ProfileModel.projects),
+        selectinload(ProfileModel.resumes),
+        selectinload(ProfileModel.soft_skills),
+        selectinload(ProfileModel.tech_stacks)
+    ).filter(ProfileModel.id == profile_id).first()
+    
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    
+    return profile
+
+@router.post("/profiles", response_model=Profile)
+def create_profile(profile: ProfileCreate, db: Session = Depends(get_db)):
+
+    existing_profile = db.query(ProfileModel).filter(ProfileModel.name.ilike(profile.name)).first()
+    if existing_profile:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Profile with this name already exists.")
+    
+    db_profile = ProfileModel(**profile.dict())
+    db.add(db_profile)
+    db.commit()
+    db.refresh(db_profile)
+    return db_profile
--- src/schemas/profile.py
+++ src/schemas/profile.py
@@ -1,15 +1,33 @@
-# src/schemas/profile.py
-
 from pydantic import BaseModel
+from typing import List, Optional
+from .certification import Certification
+from .contact import Contact
+from .past_experience import PastExperience
+from .programming_skill import ProgrammingSkill
+from .project import Project
+from .resume import Resume
+from .soft_skill import SoftSkill
+from .tech_stack import TechStack
 
 class ProfileBase(BaseModel):
     name: str
     description: str
 
-class CertificationCreate(ProfileBase):
-    id: int
+class ProfileCreate(ProfileBase):
+    pass
 
 class Profile(ProfileBase):
+    id: int
 
     class Config:
         orm_mode = True
+
+class ProfileDetail(Profile):
+    certifications: List[Certification] = []
+    contacts: List[Contact] = []
+    past_experiences: List[PastExperience] = []
+    programming_skills: List[ProgrammingSkill] = []
+    projects: List[Project] = []
+    resumes: List[Resume] = []
+    soft_skills: List[SoftSkill] = []
+    tech_stacks: List[TechStack] = []
